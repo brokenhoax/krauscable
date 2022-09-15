bb5924d3bf00c230cdac4a025e9ca62be37adbca
Andrew <brokenhoax@outlook.com>
2022-09-15 00:23:19 -0500

    add contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,17 +1,14 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../components/layout";
+import Navbar from "../components/navbar/navbar";
+import Logo from "../components/logo/logo.js";
 
 export default function Home() {
   return (
     <Layout>
-      <Head>
-        <title>Contact</title>
-      </Head>
-      <h1>Contact</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <Logo></Logo>
+      <Navbar></Navbar>
     </Layout>
   );
 }
