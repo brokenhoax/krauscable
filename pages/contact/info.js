44ed8934534b2791623d55f58312b9e9a2b7cd57
Andrew <brokenhoax@outlook.com>
2022-09-15 10:15:36 -0500

    placesaver pages

---
+++ pages/contact/info.js
@@ -0,0 +1,12 @@
+import Layout from "../../components/layout";
+import Navbar from "../../components/navbar/navbar";
+import Logo from "../../components/logo/logo.js";
+
+export default function Info() {
+  return (
+    <Layout>
+      <Logo></Logo>
+      <Navbar></Navbar>
+    </Layout>
+  );
+}
