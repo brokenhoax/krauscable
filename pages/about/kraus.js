87077e8aea0c2c0485021bae218c328a64f9cd3e
Andrew <brokenhoax@outlook.com>
2022-09-15 21:18:12 -0500

    escaping single quotes

--- pages/about/kraus.js
+++ pages/about/kraus.js
@@ -24,7 +24,7 @@ const Kraus = () => {
         Arthur “Skip” Jr. grew up in the apartment behind the radio shop where
         he learned the language of the radio by testing tubes and learning from
         the technicians. Training was being taught at the local high schools
-        and, in the late 50's, Skip was exposed to transistors, color TV and
+        and, in the late 50&#39;s, Skip was exposed to transistors, color TV and
         building master antenna systems for hospitals, apartment complexes, and
         hotels all of which laid the groundwork for whole community antenna
         systems and, eventually, Cable Television.
@@ -35,8 +35,8 @@ const Kraus = () => {
         this part of Illinois. He eventually built four more systems. Soon,
         satellite programming became a part of the cable systems. What seems to
         be true in the electronic business is constantly changing. As part of
-        what's changing is the introduction of the internet. We were the first
-        again to offer internet in this part of the state. Being a family
+        what&#39;s changing is the introduction of the internet. We were the
+        first again to offer internet in this part of the state. Being a family
         business, his two sons joined the business. Phone service was eventually
         added. Again, another change is the introduction of fiber optic cable.
       </p>
