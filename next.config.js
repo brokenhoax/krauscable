0b2b406b077a4456dc224a2cc60ebbd32603e725
Andrew <brokenhoax@outlook.com>
2022-09-15 22:45:10 -0500

    support local dev

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: "/krauscable/",
+  assetPrefix: isProd ? "/krauscable/" : undefined,
 };
 
 module.exports = nextConfig;
