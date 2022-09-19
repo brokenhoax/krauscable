a91be1ba8c848f4065c913617ff8f4b39510ddc5
Andrew <brokenhoax@outlook.com>
2022-09-19 09:53:58 -0500

    user page improvements

--- pages/profile/user.js
+++ pages/profile/user.js
@@ -7,24 +7,24 @@ export default function User() {
   return (
     <Layout>
       <Logo></Logo>
-      <section className={"section"}>
-        <h1 className={styles.header}>Account Management</h1>
+      <section className="pb-128">
+        <h1>Account Management</h1>
         {/* User */}
         <div className={styles.section}>
-          <div className={styles.primary}>
+          <div className={"space-between"}>
             <h2>User</h2>
-            <div className={"secondary-small"}>Change Password</div>
+            <div className={"link"}>Change Password</div>
           </div>
-          <div className="primary-text">brokenhoax</div>
+          <h3>brokenhoax</h3>
         </div>
         {/* Subscription */}
         <div className={styles.section}>
-          <div className={styles.primary}>
+          <div className={"space-between"}>
             <h2>Subscription</h2>
-            <div className={"secondary-small"}>Change Subscription</div>
+            <div className={"link"}>Change Subscription</div>
           </div>
-          <div className="primary-text">Kraus Triple Play</div>
-          <ul className={styles.list}>
+          <h3>Kraus Triple Play</h3>
+          <ul className={"list"}>
             <li>Cable (HBO, Cinemax)</li>
             <li>Phone (Residential)</li>
             <li>Internet (Pro Gamer)</li>
@@ -32,47 +32,54 @@ export default function User() {
         </div>
         {/* Balance Due */}
         <div className={styles.section}>
-          <div className={styles.primary}>
+          <div className={"space-between"}>
             <h2>Balance Due</h2>
-            <div className={"secondary-small"}>Pay Bill Online</div>
+            <div className={"link"}>Pay Bill Online</div>
           </div>
-          <div className={`primary-text ${styles.primary}`}>
+          <div className={"space-between"}>
             <div>August 31st, 2022</div>
             <div>-$120</div>
           </div>
         </div>
         <div className={"divider"}></div>
         {/* Account Activity */}
-        <h1 className={styles.header}>Account Activity</h1>
-        {/* Payment History */}
-        <div className={styles.primary}>
-          <h2>Payment History</h2>
-        </div>
-        <ol className={styles.list}>
-          <li className="flex-between">
-            <div>July 31, 2022</div>
-            <div>$125</div>
-          </li>
-          <li className="flex-between">
-            {" "}
-            <div>June 30, 2022</div>
-            <div>$125</div>
-          </li>
-          <li className="flex-between">
-            {" "}
-            <div>May 31, 2022</div>
-            <div>$125</div>
-          </li>
-          <li className="flex-between">
-            {" "}
-            <div>April 30, 2022</div>
-            <div>$125</div>
-          </li>
-        </ol>
-        <div className="quote">
-          Best decision I made was to switch to Kraus internet. I had AT&T and
-          for the same price with Kraus my speeds are 10x faster than AT&T.
-          <span>— Tim P.</span>
+        <h1>Account Activity</h1>
+        <div className={styles.section}>
+          {/* Payment History */}
+          <div className={"space-between"}>
+            <h2>Payment History</h2>
+          </div>
+          <ol className={styles.list}>
+            <li className="flex-between">
+              <div>July 31, 2022</div>
+              <div>$125</div>
+            </li>
+            <li className="flex-between">
+              {" "}
+              <div>June 30, 2022</div>
+              <div>$125</div>
+            </li>
+            <li className="flex-between">
+              {" "}
+              <div>May 31, 2022</div>
+              <div>$125</div>
+            </li>
+            <li className="flex-between">
+              {" "}
+              <div>April 30, 2022</div>
+              <div>$125</div>
+            </li>
+          </ol>
+          <div className={"divider"}></div>
+          <h1>Reviews</h1>
+          <div className={styles.section}>
+            <p className="quote">
+              Best decision I made was to switch to Kraus internet. I had AT&T
+              and for the same price with Kraus my speeds are 10x faster than
+              AT&T.
+              <span> &nbsp; — Tim P.</span>
+            </p>
+          </div>
         </div>
       </section>
       <Navbar></Navbar>
