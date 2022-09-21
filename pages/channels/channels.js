fbc07cf5cc789f6b2a99e66549e9c9bbe0a3179d
Andrew <brokenhoax@outlook.com>
2022-09-21 00:54:18 -0500

    channel list table basic

---
+++ pages/channels/channels.js
@@ -0,0 +1,312 @@
+export const listings = [
+  {
+    id: "1",
+    channel: "2.1",
+    description: " WBBM CBS",
+  },
+  {
+    id: "2",
+    channel: "2.2",
+    description: "STAR TV",
+  },
+  {
+    id: "3",
+    channel: "2.3",
+    description: "DABL",
+  },
+  {
+    id: "4",
+    channel: "2.4",
+    description: "FAVE TV",
+  },
+  {
+    id: "5",
+    channel: "5.1",
+    description: "WMAQ NBC",
+  },
+  {
+    id: "6",
+    channel: "5.2",
+    description: "COZI TV",
+  },
+  {
+    id: "7",
+    channel: "5.3",
+    description: "LX",
+  },
+  {
+    id: "8",
+    channel: "6",
+    description: "LOCAL CH",
+  },
+  {
+    id: "9",
+    channel: "7.1",
+    description: "WLS ABC",
+  },
+  {
+    id: "10",
+    channel: "7.2",
+    description: "LIVEWELL",
+  },
+  {
+    id: "11",
+    channel: "7.3",
+    description: "THIS",
+  },
+  {
+    id: "12",
+    channel: "9.1",
+    description: "WGN",
+  },
+  {
+    id: "13",
+    channel: "9.2",
+    description: "ANTENNA TV",
+  },
+  {
+    id: "14",
+    channel: "9.3",
+    description: "COURT TV",
+  },
+  {
+    id: "15",
+    channel: "9.4",
+    description: "TBD",
+  },
+  {
+    id: "16",
+    channel: "10",
+    description: "HLN",
+  },
+  {
+    id: "17",
+    channel: "17",
+    description: "EWTN",
+  },
+  {
+    id: "18",
+    channel: "19",
+    description: "TBD",
+  },
+  {
+    id: "19",
+    channel: "20.1",
+    description: "WYCC",
+  },
+  {
+    id: "20",
+    channel: "21",
+    description: "CSPAN",
+  },
+  {
+    id: "21",
+    channel: "22.1",
+    description: "WEATHER CH HD",
+  },
+  {
+    id: "22",
+    channel: "23",
+    description: "WEATHER CH SD",
+  },
+  {
+    id: "23",
+    channel: "26.1",
+    description: "THE CW26  WCIU",
+  },
+  {
+    id: "24",
+    channel: "26.2",
+    description: "THE U",
+  },
+  {
+    id: "25",
+    channel: "26.3",
+    description: "METV",
+  },
+  {
+    id: "26",
+    channel: "26.4",
+    description: "H&I",
+  },
+  {
+    id: "27",
+    channel: "26.5",
+    description: "BOUNCE",
+  },
+  {
+    id: "28",
+    channel: "26.6",
+    description: "DECADES",
+  },
+  {
+    id: "29",
+    channel: "32.1",
+    description: "WFLD FOX 32",
+  },
+  {
+    id: "30",
+    channel: "32.2",
+    description: "MOVIES",
+  },
+  {
+    id: "31",
+    channel: "32.3",
+    description: "BUZZER",
+  },
+  {
+    id: "32",
+    channel: "32.4",
+    description: "THE GRIO",
+  },
+  {
+    id: "33",
+    channel: "38.1",
+    description: "WCPX ION",
+  },
+  {
+    id: "34",
+    channel: "38.2",
+    description: "COURT TV MYSTERY",
+  },
+  {
+    id: "35",
+    channel: "38.3",
+    description: "COURT TV",
+  },
+  {
+    id: "36",
+    channel: "38.4",
+    description: "LAFF",
+  },
+  {
+    id: "37",
+    channel: "38.5",
+    description: "QVC",
+  },
+  {
+    id: "38",
+    channel: "38.6",
+    description: "HSN",
+  },
+  {
+    id: "39",
+    channel: "50.1",
+    description: "WPWR MY50",
+  },
+  {
+    id: "40",
+    channel: "56.1",
+    description: "WYIN  LAKESHORE PBS",
+  },
+  {
+    id: "41",
+    channel: "56.2",
+    description: "NHK",
+  },
+  {
+    id: "42",
+    channel: "44.1",
+    description: "WSNS TELEMUNDO(SP)",
+  },
+  {
+    id: "43",
+    channel: "44.2",
+    description: "TELEXITOS (SP)",
+  },
+  {
+    id: "44",
+    channel: "60.1",
+    description: "WXFT UMAS (SP)",
+  },
+  {
+    id: "45",
+    channel: "60.2",
+    description: "COURT TV MYSTERY",
+  },
+  {
+    id: "46",
+    channel: "60.3",
+    description: "QUEST",
+  },
+  {
+    id: "47",
+    channel: "62.1",
+    description: "WJYS INDEPENDENT",
+  },
+  {
+    id: "48",
+    channel: "62.2",
+    description: "STADIUM",
+  },
+  {
+    id: "49",
+    channel: "62.3",
+    description: "CHARGE!",
+  },
+  {
+    id: "50",
+    channel: "62.4",
+    description: "HEARTLAND",
+  },
+  {
+    id: "51",
+    channel: "62.5",
+    description: "QVC",
+  },
+  {
+    id: "52",
+    channel: "62.6",
+    description: "CANAL DE LA FE",
+  },
+  {
+    id: "53",
+    channel: "62.7",
+    description: "JEWELRY TV",
+  },
+  {
+    id: "54",
+    channel: "62.8",
+    description: "COMET",
+  },
+  {
+    id: "55",
+    channel: "62.9",
+    description: "HSN",
+  },
+  {
+    id: "56",
+    channel: "62.1",
+    description: "MCTV",
+  },
+  {
+    id: "57",
+    channel: "66.1",
+    description: "WGBO UNIVISION(SP)",
+  },
+  {
+    id: "58",
+    channel: "66.2",
+    description: "LAFF",
+  },
+  {
+    id: "59",
+    channel: "66.3",
+    description: "GET TV",
+  },
+  {
+    id: "60",
+    channel: "66.4",
+    description: "TRUE CRIMES",
+  },
+  {
+    id: "61",
+    channel: "66.5",
+    description: "GRIT",
+  },
+  {
+    id: "62",
+    channel: "66.6",
+    description: "TWIST",
+  },
+];
--- pages/channels/listings.js
+++ pages/channels/listings.js
@@ -1,11 +1,31 @@
 import Layout from "../../components/layout";
 import Navbar from "../../components/navbar/navbar";
 import Logo from "../../components/logo/logo.js";
+import { listings } from "./channels.js";
 
 export default function Listings() {
   return (
     <Layout>
       <Logo></Logo>
+      <h1>Account Management</h1>
+      <section className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Channel</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listings.map((x) => (
+              <tr key={x.id}>
+                <td>{x.channel}</td>
+                <td>{x.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
       <Navbar></Navbar>
     </Layout>
   );
